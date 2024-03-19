27c09aa91a318dca6be92eb2ce4bdd4127cf89f9
Hunter Dorrill <hunterdorrill@icloud.com>
2024-03-19 12:10:25 -0500

    update code with y servo and readme

--- main/main.ino
+++ main/main.ino
@@ -7,18 +7,22 @@ void setup() {
 }
 
 int ADvalueX;
+int Xpos;
 int ADvalueY;
 
 void loop() {
   ADvalueX=analogRead(A0);
+  Xpos = ADvalueX * 180.0 / 1023.0; // extra zeros to force floating point math
   ADvalueY=analogRead(A1);
   Serial.print("X=");
   Serial.print(ADvalueX);
+  Serial.print(" Xpos=");
+  Serial.print(Xpos);
   Serial.print(" Y=");
   Serial.println(ADvalueY);
   delay(10);
   
-  Xservo.write(ADvalueX * .176)
+  Xservo.write(Xpos);
 
   //Xservo.write(0);
   //delay(1000);
