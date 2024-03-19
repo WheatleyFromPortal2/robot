098723d2e4ba31d1ad7caa324343fdda7da1f853
Hunter Dorrill <hunterdorrill@icloud.com>
2024-03-19 15:27:54 -0500

    update

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 - Arduino Uno (or compatable clone)
 - [Fundiuno Shield](https://cb-electronics.com/products/funduino-joystick-shield-v1-a-ky-023-shield/)
 - 9g Micro Servo (IO Pin 9)
+# Fundiuno Schematic
 ![schematic from cb-electronics.com](schematic.png)
--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,22 @@
 #include <Servo.h>
 Servo Xservo;
+Servo Yservo;
 
 void setup() {
   Serial.begin(9600);
   Xservo.attach(9);
+  Yservo.attach(10);
 }
 
 int ADvalueX;
 int Xpos;
 int ADvalueY;
+int Ypos;
 
 void loop() {
   ADvalueX=analogRead(A0);
   Xpos = ADvalueX * 180.0 / 1023.0; // extra zeros to force floating point math
+  Ypos = ADvalueY * 180.0 / 1023.0;
   ADvalueY=analogRead(A1);
   Serial.print("X=");
   Serial.print(ADvalueX);
@@ -20,10 +24,9 @@ void loop() {
   Serial.print(Xpos);
   Serial.print(" Y=");
   Serial.println(ADvalueY);
-  delay(10);
-  
+  delay(10);  
   Xservo.write(Xpos);
-
+  Yservo.write(Ypos);
   //Xservo.write(0);
   //delay(1000);
   //Xservo.write(180);
