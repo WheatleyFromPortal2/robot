ee7c5a6ecf7ae2c46d499f5872247306e5c8e4d1
Hunter Dorrill <hunterdorrill@icloud.com>
2024-04-07 14:26:11 -0500

    remove references to old adafruit display code

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -14,25 +14,9 @@ You need to install the library titled "RF24" from the library manager.  "RF24" 
 // ---Code for U8g2 Display---
 #include <U8g2lib.h>
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R2); // define OLED display
-// ---Varaibles needed to be transmitted by the Robot--- (U8g2 display)
-int ackData[0]=28;
-int ackData[1]=67;
-int ackData[2]=10; // distance values from distance sensors. ackData[2]-ackData[4]
-int ackData[3]=20;
-int ackData[4]=30; 
-int ackData[5]=20;
-int ackData[6] =160;
-// ___end of varibles tranmittted by robot___
-/* ---Code for old Ardafruit Display---
-//#include <Adafruit_GFX.h> // include library for 0.96" OLED display
-//#include <Adafruit_SSD1306.h>  // code copied from https://electropeak.com/learn/interfacing-0-96-inch-ssd1306-oled-i2c-display-with-arduino/
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display heigh, in pixels
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SSD1306_NO_SPLASH // Disable OLED splash-screen
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Declare display
-   ___end of code for adafruit display___ */
-bool LCDinstalled = false; // if LCD installed, make this true
+int ackData[8] = {28, 67, 10, 20, 30, 20, 160, 1}; // just example values, check README.md for more info on what they actually mean
+
+bool LCDinstalled = true; // if LCD installed, make this true
 
 // io pin assignments for joystick shield
 int AnalogX = A0;
@@ -54,7 +38,6 @@ int RF_CSN = 10;
 String buf = String(ChannelFrequency);
 // Variables for recieving data from Robot, using ackData
 
-int ackData[8] = {-1, -1, -1, -1, -1, -1, -1, -1}; // -1 is just to test, check README for info on what each is for
 bool newData = false;
 unsigned long currentMillis;
 unsigned long prevMillis;
@@ -81,15 +64,10 @@ void setup() {
   while (!Serial) {
     // some boards need to wait to ensure access to serial over USB
   }
-  /*if(!display.begin(SSD1306_SWITCHCAPVCC, 0ackData[4]C)) { // Address 0ackData[4]C for 128ackData[4]2
-    Serial.println(F("SSD1306 allocation failed, reboot Ardiuno to fix, or check I2C connection"));
-    for(;;); // hold in infinite loop
-  }*/ // Old display code
   Serial.print("ChannelFrequency=");
   Serial.println(ChannelFrequency);
   Serial.print("RFpipe=");
   Serial.println(RFpipe);
-  //if (LCDinstalled) SetupLCD();
   u8g2.begin(); // Enables U8g2 display
   if (!radio.begin()) {
     Serial.println(F("radio hardware is not responding.  Please reset."));
@@ -166,88 +144,36 @@ void send() {
   }
   prevMillis = millis();
 }
-/*void SetupLCD(){ // !!!OLD CODE for Adafruit!!! Initializes display, YOU MUST CALL "display.display();" AFTER MAKING CHANGES TO UPDATE THE DISPLAY!
-  display.display();
-  delay(1000); // Pause for 1 second (maybe try lower value later?)
-  display.clearDisplay();
-  display.setTextSize(1); // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0); // Start at top-left corner
-  display.cp437(true); // Use full 256 char 'Code Page 437' font
-  // ---Boot-Message---
-  buf = String(ChannelFrequency);
-  display.println("rfFreq: ");
-  display.print(buf); // Prints the frequency
-  buf = String(RFpipe);
-  display.println("rfPipe: ");
-  display.print(buf); // Prints the rfPipe
-  buf = String(txIntervalMillis);
-  display.println("txIntervalMillis: ");
-  display.print(buf);
-  display.display();
-  delay(1000); // delay 1 second (Maybe lower later?)
-}*/
 
 void PrintToLCD() {
   gfxTime = millis();
   txPercent = (successfulTx / (successfulTx + failedTx)) * 100.0; // Calculate the successful Tx percentage, force floating point math
 
-  if (vScreen == 0){ // Prints the !!OLD!! screen, add more else-ifs for more screens!
-    /*display.clearDisplay();
-    display.setCursor(0,0);
-    display.println("Pkt:");
-    for (int x = 0; x <= 7; x++) { // Print out payload to OLED
-      buf = String(payload[x]);
-      display.print(" ");
-      display.print(buf);
-    }
-    buf = String(successfulTx);
-    display.println("Tx: ");
-    display.print(buf);
-    display.print("/");
-    buf = String(failedTx);
-    display.print(buf);
-    display.print(", ");
-    buf = String(txPercent);
-    display.print(buf);
-    display.print("%");
-    display.println("ackData:");
-    for (int x = 0; x <= 1; x++) { // Print out ackData to OLED
-      buf = ackData[x];
-      display.print(" ");
-      display.print(buf);
-    }
-    if (goodSignal) {
-      display.println("Signal Good! (>-64dBm");
-      display.invertDisplay(false); // Don't invert display if the signal is good
-    }
-    else {
-      display.println("Signal Bad! (<-64dBm");
-      display.invertDisplay(true); // Inverts entire display if signal is bad
-    }*/
+  if (vScreen == 0){ // payload vScreen
+    
   }
-    else if (vScreen == 1){
-    // this displays the proximity data
-     u8g2.drawEllipse(64, 63, 60, 54, U8G2_DRAW_UPPER_RIGHT | U8G2_DRAW_UPPER_LEFT); 
-     u8g2.setFont(u8g2_font_profont11_mf);
-     u8g2.setCursor(0, 7);
-     u8g2.print(ackData[2]);
-     u8g2.setCursor(58, 7);
-     u8g2.print(ackData[3]);
-     u8g2.setCursor(116, 7);
-     u8g2.print(ackData[4]);
-     u8g2.drawDisc(63, 60, 2); 
-     if (ackData[2]<40){
-       u8g2.drawTriangle(56-(ackData[2]*1.3), 63, 56-(ackData[2]*1.3), 51, 56-(ackData[2]*1.3)+6, 57);
-     }
-     if (ackData[4]<40){
-       u8g2.drawTriangle(68+(ackData[4]*1.3), 63, 68+(ackData[4]*1.3), 51, 68+(ackData[4]*1.3)-6, 57);
+  else if (vScreen == 1){
+  // this displays the proximity data
+    u8g2.drawEllipse(64, 63, 60, 54, U8G2_DRAW_UPPER_RIGHT | U8G2_DRAW_UPPER_LEFT); 
+    u8g2.setFont(u8g2_font_profont11_mf);
+    u8g2.setCursor(0, 7);
+    u8g2.print(ackData[2]);
+    u8g2.setCursor(58, 7);
+    u8g2.print(ackData[3]);
+    u8g2.setCursor(116, 7);
+    u8g2.print(ackData[4]);
+    u8g2.drawDisc(63, 60, 2); 
+    if (ackData[2]<40){
+     u8g2.drawTriangle(56-(ackData[2]*1.3), 63, 56-(ackData[2]*1.3), 51, 56-(ackData[2]*1.3)+6, 57);
+    }
+    if (ackData[4]<40){
+     u8g2.drawTriangle(68+(ackData[4]*1.3), 63, 68+(ackData[4]*1.3), 51, 68+(ackData[4]*1.3)-6, 57);
 
-     }
-     if (ackData[3]<40){
-       u8g2.drawTriangle(63, 60-(ackData[3]*1.15), 57, 54-(ackData[3]*1.15), 69, 54-(ackData[3]*1.15)); 
-     }
-   }
+    }
+    if (ackData[3]<40){
+     u8g2.drawTriangle(63, 60-(ackData[3]*1.15), 57, 54-(ackData[3]*1.15), 69, 54-(ackData[3]*1.15)); 
+    }
+  }
    else if (vScreen==2){
      // this displays a bar graph (most likely motor speed)//*
      int graphH1= ackData[0]*.48;
